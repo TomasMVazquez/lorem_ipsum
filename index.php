47868e8f7e2a97bb4f047511b9390efa90311b80
Maria Sol <msolpalma@gmail.com>
2019-06-01 23:26:10 -0300

    v0.3.7 Actualicé el Home (html y css)

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$title="Lorem ipsum | Home";
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <?php require_once("includes/head.php") ?>
@@ -5,55 +9,62 @@
 
     <?php require_once("includes/header.php") ?>
     <br>
-    
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="imgs/imagen1.jpg" alt="cosmetica1">
-          <div class="carousel-caption">
-          <h3 class="textoimg">imagen1</h3>
-          <p class="textoimg2">vemosquepongo1</p>
-          </div>
-        </div>
+<div class="carrousel-mobile">
+  <div id="demo" class="carousel slide" data-ride="carousel">
 
-        <div class="item">
-          <img src="imgs/imagen5.jpg" alt="cosmetica2">
-          <div class="carousel-caption">
-            <h3 class="textoimg">imagen2</h3>
-            <p class="textoimg2">vemos que pongo 2</p>
-          </div>
-        </div>
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
 
-        <div class="item">
-          <img src="imgs/imagen3.jpg" alt="cosmetica3">
-          <div class="carousel-caption">
-            <h3 class="textoimg">imagen3</h3>
-            <p class="textoimg2">vemos que pongo3</p>
-          </div>
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="imgs/imagen7.jpg" alt="img 7" width="1100" height="500">
+        <div class="carousel-caption">
+          <h3>Productos Naturales</h3>
+          <p>sdhflkjhvlahvalkjhvkshv</p>
         </div>
       </div>
-
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a>
+      <div class="carousel-item">
+        <img src="imgs/imagen1.jpg" alt="img 1" width="1100" height="500">
+        <div class="carousel-caption">
+          <h3>Cruelty Free</h3>
+          <p>adjfhdsjvkjldhv</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="imgs/items/3.jpg" alt="img 3" width="1100" height="500">
+        <div class="carousel-caption">
+          <h3>oh pos si como sea</h3>
+          <p>jbgjsdaf</p>
+        </div>
+      </div>
     </div>
+    <!-- Left and right controls -->
 
-    <br>
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+</div>
 
+  <br>
+  <br>
+
+  <h3>Nosotres</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore </p>
+
+
+
+
+  <br>
   <?php require_once("includes/footer.php") ?>
 
   </body>
